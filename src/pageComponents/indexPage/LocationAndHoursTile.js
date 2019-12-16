00d59dd9a33711ff5d2fc529586c54306f64f91f
Tomer <r3m0t@users.github.com>
2019-12-16 18:43:48 +0000

    Amend Google Maps embed

--- src/pageComponents/indexPage/LocationAndHoursTile.js
+++ src/pageComponents/indexPage/LocationAndHoursTile.js
@@ -204,8 +204,8 @@ const LocationAndHoursTile = () => {
 					</Times>
 				</InfoWrapper>
 
-				<iframe 
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2478.302179388684!2d-0.1985182842266448!3d51.599351579650374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487617521178e1cb%3A0x691313c53747e04d!2sFinchley+Osteopathy+Clinic!5e0!3m2!1sen!2suk!4v1565564708951!5m2!1sen!2suk" 
+				<iframe
+					src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9913.220282450782!2d-0.196334!3d51.5992986!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf5af54b385d4f03f!2sHealth%20Space%20307!5e0!3m2!1sen!2suk!4v1576521467030!5m2!1sen!2suk" 
 					width="95%" 
 					height="350px" 
 					frameBorder="0" 
